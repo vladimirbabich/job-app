27d67481fbb766b05d18fefab4b183ea3870e742
Vladimir Babich <vladimirbabich18@gmail.com>
2022-12-10 10:23:04 +0300

    add jwt to registration,login, add check function, add middleware for auth

--- src/server/routes/userRouter.js
+++ src/server/routes/userRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express');
 const router = new Router();
+const authMiddleware = require('../middleware/authMiddleware')
 const userController = require('../controllers/UserController');
 
 router.post('/registration', userController.registration);//ok
 router.get('/update', userController.update);//ok
-router.get('/login', userController.login);//later
-router.get('/auth', userController.check);//later
+router.get('/login', userController.login);//ok
+router.get('/auth', authMiddleware, userController.check);//later
 router.get('/getall', userController.getAll);//ok
 
 module.exports = router
